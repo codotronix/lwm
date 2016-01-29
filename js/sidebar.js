0e5093922ea9c215f9ff40e74864f873f4b57ce6
Suman Barick <codotronix@gmail.com>
2016-01-29 22:15:08 +0530

    sidebar added

---
+++ js/sidebar.js
@@ -0,0 +1,12 @@
+var sidebar = {
+    "vdo": [
+        {
+            "title": "Inkscape Tutorial - Make an Aeroplane - Beginner Level",
+            "src": "https://www.youtube.com/embed/osxtZzpV3W0"
+        },
+        {
+            "title": "My Game Mini-Puzzle for Android and Windows Phone",
+            "src": "https://www.youtube.com/embed/ZehWB5Tj6E4"
+        }
+    ]
+}
